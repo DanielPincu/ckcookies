70283cd9b299f1ebc17edc42f6530e5100ed8f58
DanielPincu <ghepin01@easv365.dk>
2025-03-24 12:04:12 +0100

    Featured products section added

--- page-home.php
+++ page-home.php
@@ -217,6 +217,33 @@
 
      </section>
 
+    <!-- Trust indicator ends  -->
+
+    <!-- Featured Products -->
+
+
+    <section class="orange relative h-96 mt-44">
+        <div class="z-10 absolute -top-5 lg:-top-14 rotate-180">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
+        </div>
+
+        <div class="relative z-10 container mx-auto py-20 px-10">
+            <h3 class="text-start lg:text-3xl text-2xl text-slate-100">Delicious Flavorous Items</h3>
+            
+            
+            
+        </div>
+
+        <div class="z-10 absolute -bottom-5 lg:-bottom-14">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
+        </div>
+    </section>
+
+
+
+
+
+
 
 
 
