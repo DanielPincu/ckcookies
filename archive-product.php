13113bd31cb027bff53f79ce4e1ba84ad2c843db
DanielPincu <ghepin01@easv365.dk>
2025-03-26 08:38:03 +0100

    Single product template added

--- archive-product.php
+++ archive-product.php
@@ -1,31 +1,22 @@
 <?php 
+get_header(); 
+// Get the shop page ID
+$shop = get_option('woocommerce_shop_page_id');
+?>
+<!-- <?php echo get_field('shop_intro', $shop) ?> -->
 
-get_header(); ?>
+<section class="orange relative h-64 mb-20 ">
+    <div class="relative z-10 container mx-auto pt-20 xl:py-32 px-10">
+        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Velkommen til vores butik</h3>
+    </div>
 
-<section class="orange relative h-96">
-        
+    <div class="z-10 absolute -bottom-5 lg:-bottom-14">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
+    </div>
+</section>
 
-        <div class="relative z-10 container mx-auto py-20 px-10">
-            <h3 class="text-start lg:text-3xl text-2xl text-slate-100">Featured products</h3>
-        </div>
 
-        <div class="z-10 absolute -bottom-5 lg:-bottom-14">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
-        </div>
-    </section>
 <div class="container mx-auto p-4">
-    
-    <!-- Display category name or Shop -->
-    <h3 class="text-3xl font-bold text-center my-8">
-        <?php 
-        if (is_product_category()) {
-            single_cat_title(); // Display the category name if on a product category page
-        } else {
-            echo 'Welcome to our shop'; // Display 'Shop' if not on a category page
-        }
-        ?>
-    </h3>
-
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
---
+++ single-product.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ */
+
+ // Prevents users from opening core PHP files directly in the browser.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+
+<section class="orange relative h-64 mb-20 ">
+    <div class="relative z-10 container mx-auto pt-20 xl:py-32 px-10">
+        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Velkommen til vores butik</h3>
+    </div>
+
+    <div class="z-10 absolute -bottom-5 lg:-bottom-14">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
+    </div>
+</section>
+
+<div class="container mx-auto px-4 py-8">
+    <?php
+    // Display the single product using the WooCommerce shortcode
+    echo do_shortcode('[product_page id="' . get_the_ID() . '"]');
+    ?>
+</div>
+
+
+
+<?php
+get_footer(); 
+?>
