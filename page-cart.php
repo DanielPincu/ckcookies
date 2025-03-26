986d500e9956f1af6b95320094d4521f053bd41a
DanielPincu <ghepin01@easv365.dk>
2025-03-26 08:54:36 +0100

    Checkout template updated, coupon display none

--- page-cart.php
+++ page-cart.php
@@ -3,6 +3,16 @@
 get_header(); // Include the header
 ?>
 
+<section class="orange relative h-64 mb-20 ">
+    <div class="relative z-10 container mx-auto pt-20 xl:py-32 px-10">
+        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Velkommen til vores butik</h3>
+    </div>
+
+    <div class="z-10 absolute -bottom-5 lg:-bottom-14">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.png" alt="" class="">
+    </div>
+</section>
+
 <div class="container mx-auto px-4 py-8">
     <h1 class="text-3xl font-bold mb-6 text-center">Your Cart</h1>
 
