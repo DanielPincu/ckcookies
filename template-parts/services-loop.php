645b1728c871c52f9950b8d6cf9a102446ad3f5d
DanielPincu <ghepin01@easv365.dk>
2025-04-28 15:01:37 +0200

    Services pointing to shop page

--- template-parts/services-loop.php
+++ template-parts/services-loop.php
@@ -10,7 +10,6 @@ $args = array(
 );
 
 $query = new WP_Query($args);
-
 ?>
 
 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 items-center justify-center container mx-auto md:px-10 pt-10 pb-32 gap-8">
@@ -20,9 +19,11 @@ $query = new WP_Query($args);
             $service_description = get_field('service_description');
             $service_icon = get_field('service_icon'); // ACF Image Field
             $service_title = get_the_title();
+            $shop_link = get_permalink( wc_get_page_id( 'shop' ) ); // Get WooCommerce Shop page URL
             ?>
             
-            <div class="relative light h-72 border-4 border-pink-600 rounded-3xl hover:text-[#FDF7EC] duration-300">
+            <!-- Wrap the whole service item in a link -->
+            <a href="<?php echo esc_url($shop_link); ?>" class="relative light h-72 border-4 border-pink-600 rounded-3xl hover:text-[#FDF7EC] duration-300">
                 <h3 class="relative text-center text-2xl pt-10 z-10"><?php echo esc_html($service_title); ?></h3>
                 <p class="relative z-10 px-5 text-sm"><?php echo esc_html($service_description); ?></p>
                 <div class="absolute z-40 -bottom-10 left-1/2 transform -translate-x-1/2 border-2 border-[#FDF7EC] rounded-full w-32 h-32 bg-pink-500">
@@ -30,7 +31,7 @@ $query = new WP_Query($args);
                         <img src="<?php echo esc_url($service_icon['url']); ?>" alt="<?php echo esc_attr($service_title); ?>" class="w-full z-10">
                     <?php endif; ?>
                 </div>
-            </div>
+            </a>
 
             <?php
         endwhile;
