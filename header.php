42ed2032fc92783116d5e0f7fbb1706969386324
DanielPincu <ghepin01@easv365.dk>
2025-05-07 13:29:56 +0200

    Mobile nav updated

--- header.php
+++ header.php
@@ -75,24 +75,31 @@ if (have_rows('ticker_items', $frontpage_id)) : ?>
 
       <!-- Cart and Account Links -->
       <div class="flex space-x-4 sm:space-x-6 pl-10">
-        <a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'cart' ) ) ); ?>" class="flex cursor-pointer flex-col items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6 text-red-950">
-            <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd"/>
-          </svg>
-          <span class="text-xs font-bold text-red-950">Kurv</span>
-        </a>
+  <!-- Cart Link -->
+  <a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'cart' ) ) ); ?>" 
+     class="flex cursor-pointer flex-col items-center justify-center 
+     <?php echo is_cart() ? 'text-orange-500 scale-110' : 'text-red-950'; ?> transition duration-300">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
+      <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd"/>
+    </svg>
+    <span class="text-xs font-bold">Kurv</span>
+  </a>
 
-        <a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?>" class="relative flex cursor-pointer flex-col items-center justify-center">
-          <span class="absolute bottom-[33px] right-1 flex h-2 w-2">
-            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-            <span class="relative inline-flex h-2 w-2 rounded-full bg-red-500"></span>
-          </span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-red-950">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
-          </svg>
-          <span class="text-xs font-bold text-red-950">Konto</span>
-        </a>
-      </div>
+  <!-- Account Link -->
+  <a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?>" 
+     class="relative flex cursor-pointer flex-col items-center justify-center 
+     <?php echo is_account_page() ? 'text-orange-500 scale-110' : 'text-red-950'; ?> transition duration-300">
+    <span class="absolute bottom-[33px] right-1 flex h-2 w-2">
+      <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+      <span class="relative inline-flex h-2 w-2 rounded-full bg-red-500"></span>
+    </span>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
+    </svg>
+    <span class="text-xs font-bold">Konto</span>
+  </a>
+</div>
+
 
       <!-- Mobile Navigation Button -->
       <div class="lg:hidden z-10 pl-5">
