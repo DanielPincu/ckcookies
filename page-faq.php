ecaeaff257cd4431d2d594cb9b2907ccf9e65ab1
DanielPincu <ghepin01@easv365.dk>
2025-05-14 08:56:36 +0200

    Clean-up

--- page-faq.php
+++ page-faq.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 <section class="orange relative h-64 md:mb-44 mb-20">
     <div class="relative z-10 container mx-auto pt-20 xl:py-32 px-10">
-        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Ofte stillede spørgsmål</h3>
+        <h1 class="text-center lg:text-3xl text-2xl text-slate-100">Ofte stillede spørgsmål</h1>
     </div>
 
     <div class="z-10 absolute -bottom-5 lg:-bottom-14">
--- page-kontakt.php
+++ page-kontakt.php
@@ -1,11 +1,11 @@
 <?php get_header();?>
 <section class="orange relative h-64 mb-44 ">
     <div class="relative z-10 container mx-auto pt-20 xl:py-32 px-10">
-        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Velkommen til vores butik</h3>
+        <h3 class="text-center lg:text-3xl text-2xl text-slate-100">Kontakt Os</h3>
     </div>
 
     <div class="z-10 absolute -bottom-5 lg:-bottom-14">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.webp" alt="bølge" class="">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/wave.webp" alt="bølge">
     </div>
 </section>
 
